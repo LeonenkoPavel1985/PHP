64fdf9c509eb165ce92fe821bc6cd56ebdd7a834
Pavel Leonenko <leonenkopavel1985@gmail.com>
2023-11-09 13:43:27 +0700

    Task 14 start.

--- Part20WorkingWithFunctionsInPHP/index.php
+++ Part20WorkingWithFunctionsInPHP/index.php
@@ -204,4 +204,7 @@ function sqlDateFormatNormal($sqlDate, $chose) {
 $resultPhpDate = sqlDateFormatNormal('2023-01-31', 3);
 echo $resultPhpDate . '<br>';
 
+// 14. Сделайте функцию, которая принимает дату в нормальном формате (31.12.2023), а возвращает в sql формате. Совет: воспользуйтесь функцией explode.
+
+
 ?>
