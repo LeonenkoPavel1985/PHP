85e1470a336cf86e1cd998358cfbb096c3976ba5
Pavel Leonenko <leonenkopavel1985@gmail.com>
2023-11-13 13:05:15 +0700

    Task 22 (DONE).

--- Part20WorkingWithFunctionsInPHP/index.php
+++ Part20WorkingWithFunctionsInPHP/index.php
@@ -330,4 +330,40 @@ function daysNumber($dayName) {
 $resultmonthsNumbers = daysNumber('Monday');
 echo $resultmonthsNumbers . ' день недели.' . '<br>';
 
+// 22. Сделайте функцию, которая принимает параметром число, а возвращает день недели на определенном языке. Язык должен указываться вторым параметром ('ru'
+// — русский, 'en' — англ., 'ge' — немецкий). По умолчанию функция должна возвращать русское название (DONE).
+
+function languageDays($day, $lang = 'ru') {
+
+    if ($lang == 'ru') {
+        $days = array('1' => 'Понедельник', '2' => 'Втоник', '3' => 'Среда', '4' => 'Четверг',
+        '5' => 'Пятница', '6' => 'Суббота', '7' => 'Воскресенье');
+        if ($day >= 1 and $day <= 7) {
+            return $days[$day] . '<br>';
+        } else {
+            return 'Не верно указан день.';
+        }
+    } else if ($lang == 'en') {
+        if ($day >= 1 and $day <= 7) {
+            $days = array('1' => 'Monday', '2' => 'Tuesday', '3' => 'Wednesday', '4' => 'Thursday',
+        '5' => 'Friday', '6' => 'Saturday', '7' => 'Sunday');
+            return $days[$day] . '<br>';
+        } else {
+            return 'Не верно указан день.';
+        }
+    } else if ($lang == 'ge') {
+        if ($day >= 1 and $day <= 7) {
+            $days = array('1' => 'Montag', '2' => 'Dienstag', '3' => 'Mittwoch', '4' => 'Donnerstag',
+        '5' => 'Freitag', '6' => 'Samstag', '7' => 'Sonntag');
+            return $days[$day] . '<br>';
+        } else {
+            return 'Не верно указан день.';
+        }
+    }
+}
+
+echo languageDays(4, 'ru');
+echo languageDays(2, 'en');
+echo languageDays(7, 'ge');
+
 ?>
