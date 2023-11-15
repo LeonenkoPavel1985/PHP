bfb40891bb58ff86d4191370e31d354f1cb7b223
Pavel Leonenko <leonenkopavel1985@gmail.com>
2023-11-15 14:17:11 +0700

    Task 7 and 8 (DONE).

--- Part22AdvancedWorkWithFormsTasks/index.php
+++ Part22AdvancedWorkWithFormsTasks/index.php
@@ -2,7 +2,17 @@
 
 // Задачи.
 
-// 7. Сделайте функцию, которая создает инпут type text или password. Функция должна иметь следующие параметры: type, name, value, placeholder.
+// 7. Сделайте функцию, которая создает инпут type text или password. Функция должна иметь следующие параметры: type, name, value, placeholder (DONE).
+// 8. Модифицируйте функцию из предыдущей задачи так, чтобы она сохраняла значение инпута после отправки (DONE).
 
+
+function inputText($type, $name, $value, $placeholder) {
+    if (isset($_REQUEST['name'])) {
+        $value = $_REQUEST['name'];
+    }
+    return '<input type="' . $type . '" name="' . $name . '" value="' . $value . '" placeholder="' . $placeholder . '">';
+}
+
+echo inputText('text', 'name', '2', 'Введите текст');
 
 ?>
