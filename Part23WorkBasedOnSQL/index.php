70d2630855ce2c304ddee791d397a5518eb06751
Pavel Leonenko <leonenkopavel1985@gmail.com>
2023-11-17 10:41:26 +0700

    Разобраться с подключением е БД.

--- Part23WorkBasedOnSQL/index.php
+++ Part23WorkBasedOnSQL/index.php
@@ -1,5 +1,20 @@
 <?php
 
+// Соединение с базой данных.
 
+$servername = 'MSI\SQLEXPRESS'; // instance и порт - необязательные параметры
+$options = [
+    'UID' => 'PavelL',
+    // имя пользователя, имеющего доступ к БД
+    'PWD' => 'root',
+    // пароль
+    'Database' => 'PhpWorkersDb' // название БД, к которой подключаемся
+];
+$conn = sqlsrv_connect($servername, $options);
+if ($conn === false) {
+    die(print_r(sqlsrv_errors()));
+} else {
+    echo 'Соединение установлено';
+}
 
 ?>
