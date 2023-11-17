0d0d404a7f6574e51e86f4c52e49a4b2f7e7b52e
Pavel Leonenko <leonenkopavel1985@gmail.com>
2023-11-17 14:27:08 +0700

    Part 24 correct.

--- Part24WorkWithSessionsAndCookies/test.php
+++ Part24WorkWithSessionsAndCookies/test.php
@@ -6,8 +6,9 @@ session_start(); // Запуск сессии.
 //Если есть данные в сессии о стране.
 
 if (isset($_SESSION['country'])) {
-    echo $_SESSION['country'];  // Вывод страны на страницу.
+    echo $_SESSION['country']; // Вывод страны на страницу.
     echo '<br /><a href="index.php">index</a>';
 }
 
 ?>
+
