c4d5851570a88c7ba9c6bcb81449a5d0e7f07a01
Pavel Leonenko <leonenkopavel1985@gmail.com>
2023-11-17 12:44:19 +0700

    Task 1 (DONE).

---
+++ Part24WorkWithSessionsAndCookies/test.php
@@ -0,0 +1,13 @@
+
+<?php
+
+session_start(); // Запуск сессии.
+
+//Если есть данные в сессии о стране.
+
+if (isset($_SESSION['country'])) {
+    echo $_SESSION['country'];  // Вывод страны на страницу.
+    echo '<br /><a href="index.php">index</a>';
+}
+
+?>
