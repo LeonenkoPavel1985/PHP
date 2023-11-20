4cf8849c44f5b290b44c2894e3707e9e6e2ac893
Pavel Leonenko <leonenkopavel1985@gmail.com>
2023-11-20 11:31:48 +0700

    Task 3 (DONE).

--- Part24WorkWithSessionsAndCookies/Index.php
+++ Part24WorkWithSessionsAndCookies/Index.php
@@ -24,10 +24,49 @@ if (isset($_REQUEST['country'])) { // При условии если поле з
 
 session_start();
 
-if (empty($_SESSION['time'])) {
+if (!empty($_SESSION['time'])) {
     $_SESSION['time'] = time();
 }
 
 echo 'Пользователь заходил на сайт: ' . time() - $_SESSION['time'] . ' секунд назад.' . '<br>';
 
 ?>
+
+<!--3. Спросите у пользователя email с помощью формы. Затем сделайте так, чтобы в другой форме (имя, фамилия, пароль, email) при ее открытии поле email было автоматически заполнено (DONE).-->
+
+<form action="" method="get">
+    <p>Введите ваш email:<input type="text" name="email" /></p>
+    <p><input type="submit" value="Подтвердить" /></p>
+    <p></p>
+</form>
+
+<?php
+
+if(!empty($_SESSION['email'])) { // При условии если поле заполнено.
+    session_start();
+    $_SESSION['email'] = $_REQUEST['email']; // Запись email в сессию.
+}
+
+?>
+
+<?php
+
+session_start();
+
+// Если телефон email есть в сессии записываем его в переменную $email.
+if (!empty($_SESSION['email'])) {
+    $email = $_SESSION['email'];
+} else {
+    $email = '';
+}
+
+?>
+
+<form action="" method="get">
+    <p>Введите имя: <input type="text" name="name" /></p>
+    <p>Введите фамилию: <input type="text" name="surname" /></p>
+    <p>Введите пароль: <input type="text" name="password" /></p>
+    <!-- Заполняем атрибут value переменной $email.-->
+    <p>Введите email: <input type="text" name="email" value="<?php echo $email ?>" /></p>
+    <p><input type="submit" value="Подтвердить" /></p>
+</form>
