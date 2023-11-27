5e1730c4afa91306553b8d69621ae87be34e0a9c
Pavel Leonenko <leonenkopavel1985@gmail.com>
2023-11-27 11:21:30 +0700

    Task 1 (DONE).

--- Part27OOP/index.php
+++ Part27OOP/index.php
@@ -1,5 +1,39 @@
 <?php
 
+// Работа с классами.
 
+// 1. Сделайте класс Power, который возводит число в нужную степень (не используя стандартную функцию pow). Класс должен иметь следующие методы: 2, 3, 4, 5 степени (DONE).
+
+class Power
+{
+    public $number = 2;
+
+    function secondDegree()
+    {
+        echo 'Указанное число во второй степени: ' . $this->number * 2 . '.' . '<br>';
+    }
+
+    function thirdDegree()
+    {
+        echo 'Указанное число в третьей степени: ' . $this->number * 2 * 2 . '.' . '<br>';
+    }
+
+    function fourthDegree()
+    {
+        echo 'Указанное число в четвертой степени: ' . $this->number * 2 * 2 * 2 . '.' . '<br>';
+    }
+
+    function fifthDegree()
+    {
+        echo 'Указанное число в пятой степени: ' . $this->number * 2 * 2 * 2 * 2 . '.' . '<br>';
+    }
+}
+
+$power = new Power();
+
+$power->secondDegree();
+$power->thirdDegree();
+$power->fourthDegree();
+$power->fifthDegree();
 
 ?>
