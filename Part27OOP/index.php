a4a763901f3300aeb801ad6c0aa0c4e69ea0dcd2
Pavel Leonenko <leonenkopavel1985@gmail.com>
2023-11-28 11:22:16 +0700

    Task 2 (DONE).

--- Part27OOP/index.php
+++ Part27OOP/index.php
@@ -36,4 +36,38 @@ $power->thirdDegree();
 $power->fourthDegree();
 $power->fifthDegree();
 
+// 2. Сделайте класс-калькулятор. Класс должен иметь следующие методы: сложение, вычитание, умножение, деление. Каждый метод должен принимать параметром два числа (DONE).
+
+class calculator {
+    private $result;
+
+    function addition($number_1, $number_2) {
+        $this->result = $number_1 + $number_2;
+        echo 'Результат сложения двух чисел равна: ' . $this->result . '.' . '<br>';
+
+    }
+
+    function subtraction($number_1, $number_2) {
+        $this->result = $number_1 - $number_2;
+        echo 'Результат вычитания двух чисел равен: ' . $this->result . '.' . '<br>';
+    }
+
+    function multiplication($number_1, $number_2) {
+        $this->result = $number_1 * $number_2;
+        echo 'Результат умножения двух чисел равен: ' . $this->result . '.' . '<br>';
+    }
+
+    function division($number_1, $number_2) {
+        $this->result = $number_1 / $number_2;
+        echo 'Результат деления двух чисел равен: ' . $this->result . '.' . '<br>';
+    }
+}
+
+$calculator = new Calculator();
+
+$calculator->addition(2, 3);
+$calculator->subtraction(10, 6);
+$calculator->multiplication(3, 3);
+$calculator->division(12, 6);
+
 ?>
