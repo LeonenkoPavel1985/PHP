0c8327f136ce47d6ca4937fd6a26efb81c135aaa
Pavel Leonenko <leonenkopavel1985@gmail.com>
2023-12-18 11:20:43 +0700

    Task 11 (DONE).

--- Part28OOPTasks/index.php
+++ Part28OOPTasks/index.php
@@ -36,4 +36,104 @@ class DatabaseShell
     }
 }
 
+// 11. Сделайте класс, который находит сумму квадратов всех элементов массива и извлекает квадратный корень, сумму кубов и извлекает кубический корень и так далее до 5-той степени.
+// Класс должен наследовать от класса Sqrt, для возведения в степень использовать стандартную функцию pow (DONE).
+
+class Sqrt
+{
+    public $sumElemArr;
+    private $result;
+
+    function secondRoot() {
+        $this->result = pow($this->sumElemArr, 0.5);
+        echo 'Квадратный корень суммы элементов массива возведенных в квадрат равен: ' . $this->result . '.' . '<br>';
+    }
+
+    function thirdDegree() {
+        $this->result = pow($this->sumElemArr, 1/3);
+        echo 'Кубический корень суммы элементов массива возведенных в куб равен: ' . $this->result . '.' . '<br>';
+    }
+
+    function fourthDegree() {
+        $this->result = pow($this->sumElemArr, 1/4);
+        echo 'Корень четвертой степени суммы элементов массива возведенных в четвертую степень равен: ' . $this->result . '.' . '<br>';
+    }
+
+    function fifthDegree() {
+        $this->result = pow($this->sumElemArr, 1/5);
+        echo 'Корень в пятой степени суммы элементов массива возведенных в пятую степень равен: ' . $this->result . '.' . '<br>';
+    }
+}
+
+class SumSquare extends Sqrt
+{
+    public function sumSquaresElemArr($arr = []) {
+        $length = count($arr);
+
+        for ($i = 0; $i < $length; $i++) {
+            $arr[$i] = pow($arr[$i], 2);
+        }
+
+        echo 'Массив с элементамми возведенными в квадрат: ' . '<br>';
+        var_dump($arr);
+        echo '<br>';
+        $this->sumElemArr = array_sum($arr);
+        echo 'Сумма элементов массива с элементами возведенными в квадрат равна: ' . $this->sumElemArr . '.' . '<br>';
+        $this->secondRoot();
+    }
+
+    public function sumCubesElemArr($arr = []) {
+        $length = count($arr);
+
+        for ($i = 0; $i < $length; $i++) {
+            $arr[$i] = pow($arr[$i], 3);
+        }
+
+        echo 'Массив с элементамми возведенными в куб: ' . '<br>';
+        var_dump($arr);
+        echo '<br>';
+        $this->sumElemArr = array_sum($arr);
+        echo 'Сумма элементов массива с элементами возведенными в куб равна: ' . $this->sumElemArr . '.' . '<br>';
+        $this->thirdDegree();
+    }
+
+    public function sumFourthElemArr($arr = []) {
+        $length = count($arr);
+
+        for ($i = 0; $i < $length; $i++) {
+            $arr[$i] = pow($arr[$i], 4);
+        }
+
+        echo 'Массив с элементамми возведенными в четвертую степень: ' . '<br>';
+        var_dump($arr);
+        echo '<br>';
+        $this->sumElemArr = array_sum($arr);
+        echo 'Сумма элементов массива с элементами возведенными в четвертую степень равна: ' . $this->sumElemArr . '.' . '<br>';
+        $this->fourthDegree();
+    }
+
+    public function sumFifthElemArr($arr = []) {
+        $length = count($arr);
+
+        for ($i = 0; $i < $length; $i++) {
+            $arr[$i] = pow($arr[$i], 4);
+        }
+
+        echo 'Массив с элементамми возведенными в пятую степень: ' . '<br>';
+        var_dump($arr);
+        $this->sumElemArr = array_sum($arr);
+        echo '<br>';
+        echo 'Сумма элементов массива с элементами возведенными в пятую степень равна: ' . $this->sumElemArr . '.' . '<br>';
+        $this->fifthDegree();
+    }
+
+}
+
+$sumSquare = new SumSquare();
+$sumSquare->sumSquaresElemArr([2,2,2,2]);
+$sumSquare->sumCubesElemArr([3, 3, 3, 3]);
+$sumSquare->sumFourthElemArr([4, 4, 4, 4]);
+$sumSquare->sumFifthElemArr([5, 5, 5, 5]);
+
 ?>
+
