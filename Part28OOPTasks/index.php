a449ebe10a74f773c9f38e671004195e4bf16ef5
Pavel Leonenko <leonenkopavel1985@gmail.com>
2024-01-12 13:55:14 +0700

    Task 12 (DONE).

--- Part28OOPTasks/index.php
+++ Part28OOPTasks/index.php
@@ -136,5 +136,111 @@ $sumSquare->sumFourthElemArr([4, 4, 4, 4]);
 $sumSquare->sumFifthElemArr([5, 5, 5, 5]);
 
 echo '<br>';
-?>
 
+// 12. Сделайте класс для работы с датами. Класс должен уметь находить разницу между двумя датами, принимать дату в sql-формате, а возвращать в заданном, принимать дату в формате
+// '31.12.2023', а возвращать в заданном. Также класс должен должен определять текущий день недели и месяц (словом, по-русски) и иметь для этого соответствующие методы.
+// Класс должен иметь public свойство today, в котором хранится текущая дата (заполняется в __construct). Класс должен иметь public свойство weekday, в котором хранится текущий день недели
+// (по-русски). Класс должен иметь public свойство month, в котором хранится текущий месяц (по-русски). Класс должен иметь и использовать метод, который принимает количество секунд
+// $num, а возращает массив, в котором содержится количество лет, месяцев, дней, часов, минут, секунд в $num. Добавьте несколько методов на свой вкус (DONE).
+
+class WorkingWithDates {
+    public $today;
+    public $weekday;
+    public $month;
+
+    function __construct($today) {
+        $this->today = $today;
+    }
+
+    function getCurrentDate() {
+        echo 'Текущая дата: ' . $this->today . '.' . '<br>';
+    }
+
+    function differenceDatesToday($dateEnd) {
+        $difference = abs(strtotime($dateEnd) - strtotime($this->today));
+        $years = floor($difference / (365 * 60 * 60 * 24));
+        $months = floor(($difference - $years * 365 * 60 * 60 * 24) / (30 * 60 * 60 * 24));
+        $days = floor(($difference - $years * 365 * 60 * 60 * 24 - $months * 30 * 60 * 60 * 24) / (60 * 60 * 24));
+        echo 'Разница между датами от текущего дня равна: ' . $days . ' дня(-ей),' . $months . ' месяца(-ев),' . $years . ' год(лет).' . '<br>';
+    }
+
+    function differenceDatesTwoDate($dateStart, $dateEnd) {
+        $difference = abs(strtotime($dateEnd) - strtotime($dateStart));
+        $years = floor($difference / (365 * 60 * 60 * 24));
+        $months = floor(($difference - $years * 365 * 60 * 60 * 24) / (30 * 60 * 60 * 24));
+        $days = floor(($difference - $years * 365 * 60 * 60 * 24 - $months * 30 * 60 * 60 * 24) / (60 * 60 * 24));
+        echo 'Разница между заданными датами равна: ' . $days . ' дня(-ей),' . $months . ' месяца(-ев),' . $years . ' год(лет).' . '<br>';
+    }
+
+    function sqlFormatDate($dateSql) {
+        $dateNormal = date('d.m.Y', strtotime($dateSql));
+        echo 'Дата формата sql переведенная в заданный формат: ' . $dateNormal . '.' . '<br>';
+    }
+
+    function normalFormatDate($dateNormal) {
+        $dateSql = date('Y-m-d', strtotime($dateNormal));
+        echo 'Дата нормального формата переведенная в заданный формат sql: ' . $dateSql . '.' . '<br>';
+
+    }
+
+    function rusDayWeek() {
+        $week = array('Воскресенье', 'Понедельник', 'Втоник', 'Среда', 'Четверг', 'Пятница', 'Суббота');
+        $this->weekday = $week[date('w', time())];
+        echo 'Текущий день недели: ' . $this->weekday . '.' . '<br>';
+    }
+
+    function rusMonths() {
+        $month = ['0', 'Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
+        $this->$month = $month[date('n', time())];
+        echo 'Текущий месяц: ' . $this->$month . '.' . '<br>';
+    }
+
+    function secondDate($num) {
+        $secondsInMinutes = 60;
+        $secondsInHours = 60 * $secondsInMinutes;
+        $secondsInDays = 24 * $secondsInHours;
+
+        // Расчет дней.
+        $days = floor($num / $secondsInDays);
+
+        // Расчет часов.
+        $hourSeconds = $num % $secondsInDays;
+        $hours = floor($hourSeconds / $secondsInHours);
+
+        // Расчет минут.
+        $minuteSeconds = $hourSeconds % $secondsInHours;
+        $minutes = floor($minuteSeconds / $secondsInMinutes);
+
+        // Расчет оставшихся секунд.
+        $remainingSeconds = $minuteSeconds % $secondsInMinutes;
+        $seconds = ceil($remainingSeconds);
+
+        // Форматирование и возврат значения массива.
+        $timeParts = [];
+        $sections = [
+            'день(-я,-ей)' => (int) $days,
+            'час(-а,-ов)' => (int) $hours,
+            'минут(-ы)' => (int) $minutes,
+            'секунд(-ы)' => (int) $seconds,
+        ];
+
+        foreach ($sections as $name => $value) {
+            if ($value > 0) {
+                $timeParts[] = $value . ' ' . $name . ($value == 1 ? '' : '');
+            }
+        }
+        echo 'В указанном в секундах интервале: ' .  implode(', ', $timeParts) . '.' . '<br>';
+    }
+}
+
+$workingWithDates = new WorkingWithDates(date('d.m.Y'));
+$workingWithDates->getCurrentDate();
+$workingWithDates->differenceDatesToday('25.12.2024');
+$workingWithDates->differenceDatesTwoDate('15.06.2023', '20.12.2024');
+$workingWithDates->sqlFormatDate('2023-11-02');
+$workingWithDates->normalFormatDate('02.11.2023');
+$workingWithDates->rusDayWeek();
+$workingWithDates->rusMonths();
+$workingWithDates->secondDate(5500000);
+
+?>
